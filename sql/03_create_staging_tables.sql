9b33b3f10972d7783c74cec4eeb9bbc724f24180
Mihajlo Stankovic <mihajlo.stankovic1212@gmail.com>
2026-06-17 12:41:03 +0200

    Add orders staging table

--- sql/03_create_staging_tables.sql
+++ sql/03_create_staging_tables.sql
@@ -9,3 +9,15 @@ CREATE TABLE stg.customers (
     customer_state CHAR(2)
 );
 GO
+
+CREATE TABLE stg.orders (
+    order_id NVARCHAR(50),
+    customer_id NVARCHAR(50),
+    order_status NVARCHAR(50),
+    order_purchase_timestamp DATETIME2,
+    order_approved_at DATETIME2,
+    order_delivered_carrier_date DATETIME2,
+    order_delivered_customer_date DATETIME2,
+    order_estimated_delivery_date DATETIME2
+);
+GO
