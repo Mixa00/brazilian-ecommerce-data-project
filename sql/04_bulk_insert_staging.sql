95a1aac578e985215d3abdb7a1cb96c9840cad92
Mihajlo Stankovic <mihajlo.stankovic1212@gmail.com>
2026-06-18 11:42:23 +0200

    Add sellers bulk insert

--- sql/04_bulk_insert_staging.sql
+++ sql/04_bulk_insert_staging.sql
@@ -60,3 +60,15 @@ WITH (
     TABLOCK
 );
 GO
+
+BULK INSERT stg.sellers
+FROM 'C:\Users\mihajlo.stankovic\Documents\Codex\brazilian_e-commerce_project\data\raw\olist_sellers_dataset.csv'
+WITH (
+    FORMAT = 'CSV',
+    FIRSTROW = 2,
+    FIELDQUOTE = '"',
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '0x0a',
+    TABLOCK
+);
+GO
