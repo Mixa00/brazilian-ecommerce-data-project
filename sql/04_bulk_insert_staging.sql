5bdb164ba8b679eb4384e60ea70d81a1f6cc0648
Mihajlo Stankovic <mihajlo.stankovic1212@gmail.com>
2026-06-18 11:25:26 +0200

    Add order payments bulk insert

--- sql/04_bulk_insert_staging.sql
+++ sql/04_bulk_insert_staging.sql
@@ -36,3 +36,15 @@ WITH (
     TABLOCK
 );
 GO
+
+BULK INSERT stg.order_payments
+FROM 'C:\Users\mihajlo.stankovic\Documents\Codex\brazilian_e-commerce_project\data\raw\olist_order_payments_dataset.csv'
+WITH (
+    FORMAT = 'CSV',
+    FIRSTROW = 2,
+    FIELDQUOTE = '"',
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '0x0a',
+    TABLOCK
+);
+GO
