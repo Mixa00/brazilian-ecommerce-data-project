8ca6fc0677b01f9079c2c0da3cd9c41ea06798ff
Mihajlo Stankovic <mihajlo.stankovic1212@gmail.com>
2026-06-18 11:11:25 +0200

    Add order items bulk insert

--- sql/04_bulk_insert_staging.sql
+++ sql/04_bulk_insert_staging.sql
@@ -24,3 +24,15 @@ WITH (
     TABLOCK
 );
 GO
+
+BULK INSERT stg.order_items
+FROM 'C:\Users\mihajlo.stankovic\Documents\Codex\brazilian_e-commerce_project\data\raw\olist_order_items_dataset.csv'
+WITH (
+    FORMAT = 'CSV',
+    FIRSTROW = 2,
+    FIELDQUOTE = '"',
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '0x0a',
+    TABLOCK
+);
+GO
