44a6a2d8ea0cbf0618071ed59e7082440b37643a
Mihajlo Stankovic <mihajlo.stankovic1212@gmail.com>
2026-06-29 09:13:05 +0200

    Add referential integrity checks

--- sql/06_data_quality_checks.sql
+++ sql/06_data_quality_checks.sql
@@ -137,3 +137,82 @@ SELECT 'stg.order_payments.payment_sequential', COUNT(*)
 FROM stg.order_payments
 WHERE payment_sequential IS NULL;
 GO
+
+-- Referential integrity checks
+-- stg.orders.customer_id -> stg.customers.customer_id
+SELECT
+	o.customer_id
+FROM
+	stg.orders AS o
+	LEFT JOIN stg.customers AS c ON o.customer_id = c.customer_id
+WHERE
+	c.customer_id IS NULL;
+GO
+
+-- Alternative version
+/*
+SELECT
+	o.customer_id
+FROM
+	stg.orders AS o
+WHERE NOT EXISTS (
+	SELECT
+		1
+	FROM
+		stg.customers AS c
+	WHERE
+		o.customer_id = c.customer_id
+);
+GO
+*/
+
+-- stg.order_items.product_id -> stg.products.product_id
+SELECT
+	oi.product_id
+FROM
+	stg.order_items AS oi
+	LEFT JOIN stg.products AS p ON oi.product_id = p.product_id
+WHERE
+	p.product_id IS NULL;
+GO
+
+-- stg.order_items.seller_id -> stg.sellers.seller_id
+SELECT
+	oi.seller_id
+FROM
+	stg.order_items AS oi
+	LEFT JOIN stg.sellers AS s ON oi.seller_id = s.seller_id
+WHERE
+	s.seller_id IS NULL;
+GO
+
+-- stg.order_items.order_id -> stg.orders.order_id
+SELECT
+	oi.order_id
+FROM
+	stg.order_items AS oi
+	LEFT JOIN stg.orders AS o ON oi.order_id = o.order_id
+WHERE
+	o.order_id IS NULL;
+GO
+
+-- stg.order_payments.order_id -> stg.orders.order_id
+SELECT
+	op.order_id
+FROM
+	stg.order_payments AS op
+	LEFT JOIN stg.orders AS o ON op.order_id = o.order_id
+WHERE
+	o.order_id IS NULL;
+GO
+
+-- stg.products.product_category_name -> stg.product_category_name_translation.product_category_name
+SELECT DISTINCT
+	p.product_category_name
+FROM
+	stg.products AS p
+	LEFT JOIN stg.product_category_name_translation AS pcnt ON p.product_category_name = pcnt.product_category_name
+WHERE
+	p.product_category_name IS NOT NULL
+	AND pcnt.product_category_name IS NULL;
+GO
