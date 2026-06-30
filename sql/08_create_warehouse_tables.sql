b3ea081be5a04c6e064cceff2b9dcd4061c04e28
Mihajlo Stankovic <mihajlo.stankovic1212@gmail.com>
2026-06-30 11:05:49 +0200

    Add product dimension table

--- sql/08_create_warehouse_tables.sql
+++ sql/08_create_warehouse_tables.sql
@@ -34,3 +34,20 @@ CREATE TABLE dw.dim_sellers (
 	CONSTRAINT PK_dim_sellers PRIMARY KEY(seller_key)
 );
 GO
+
+CREATE TABLE dw.dim_products (
+	product_key INT IDENTITY(1, 1) NOT NULL,
+	product_id NVARCHAR(50) NOT NULL,
+	product_category_name NVARCHAR(100) NULL,
+	product_category_name_english NVARCHAR(100) NULL,
+	product_name_lenght INT NULL,
+	product_description_lenght INT NULL,
+	product_photos_qty INT NULL,
+	product_weight_g INT NULL,
+	product_length_cm INT NULL,
+	product_height_cm INT NULL,
+	product_width_cm INT NULL,
+
+	CONSTRAINT PK_dim_products PRIMARY KEY(product_key)
+);
+GO
