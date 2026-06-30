cf4169be811d99fdc14939ed088063443edbc9f6
Mihajlo Stankovic <mihajlo.stankovic1212@gmail.com>
2026-06-30 11:11:33 +0200

    Add product dimension load

--- sql/09_load_warehouse_dimensions.sql
+++ sql/09_load_warehouse_dimensions.sql
@@ -1,6 +1,6 @@
 USE BrazilianECommerce;
 GO
-
+-- ============================
 INSERT INTO dw.dim_customers (
 	customer_id,
 	customer_unique_id,
@@ -17,7 +17,7 @@ SELECT
 FROM
 	stg.customers;
 GO
-
+-- ============================
 INSERT INTO dw.dim_sellers (
 	seller_id,
 	seller_zip_code_prefix,
@@ -32,3 +32,30 @@ SELECT
 FROM
 	stg.sellers;
 GO
+-- ============================
+INSERT INTO dw.dim_products (
+	product_id,
+	product_category_name,
+	product_category_name_english,
+	product_name_lenght,
+	product_description_lenght,
+	product_photos_qty,
+	product_weight_g,
+	product_length_cm,
+	product_height_cm,
+	product_width_cm
+)
+SELECT
+	p.product_id,
+	p.product_category_name,
+	COALESCE(t.product_category_name_english, p.product_category_name) AS product_category_name_english,
+	p.product_name_lenght,
+	p.product_description_lenght,
+	p.product_photos_qty,
+	p.product_weight_g,
+	p.product_length_cm,
+	p.product_height_cm,
+	p.product_width_cm
+FROM
+	stg.products as p
+	LEFT JOIN stg.product_category_name_translation as t ON p.product_category_name = t.product_category_name
