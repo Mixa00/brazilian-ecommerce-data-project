3de4261ceebdf0d5e0673b84989a05a9ddde85db
Mihajlo Stankovic <mihajlo.stankovic1212@gmail.com>
2026-07-02 13:23:54 +0200

    Add revenue by customer state query

--- sql/13_analytics_queries.sql
+++ sql/13_analytics_queries.sql
@@ -1,7 +1,7 @@
 USE BrazilianECommerce;
 GO
 
--- Categories and their revenue
+-- Revenue by product category
 SELECT
 	p.product_category_name_english,
 	COUNT(*) AS order_item_count,
@@ -12,6 +12,21 @@ FROM
 	JOIN dw.dim_products AS p ON oi.product_key = p.product_key
 GROUP BY
 	p.product_category_name_english
+ORDER BY
+	total_revenue DESC;
+GO
+
+-- Revenue by customer state
+SELECT
+	c.customer_state,
+	COUNT(*) AS order_item_count,
+	SUM(oi.price) AS total_revenue,
+	SUM(oi.freight_value) AS total_freight
+FROM
+	dw.fact_order_items AS oi
+	JOIN dw.dim_customers AS c ON oi.customer_key = c.customer_key
+GROUP BY
+	c.customer_state
 ORDER BY
 	total_revenue DESC;
 GO
