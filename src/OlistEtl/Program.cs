e0ec2b93434b7ca5d221c409cf3fa40fab8e07de
Mihajlo Stankovic <mihajlo.stankovic1212@gmail.com>
2026-07-06 12:48:32 +0200

    Add CSV directory profiling

--- src/OlistEtl/Program.cs
+++ src/OlistEtl/Program.cs
@@ -2,21 +2,42 @@
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
             if (args.Length == 0)
             {
-                Console.WriteLine("Please provide a CSV file path.");
+                Console.WriteLine("Please provide a CSV file path or directory path.");
                 return;
             }
 
-            if (!File.Exists(args[0]))
+            if (!File.Exists(args[0]) && !Directory.Exists(args[0]))
             {
-                Console.WriteLine($"File not found: {args[0]}");
+                Console.WriteLine($"File or directory not found: {args[0]}");
                 return;
             }
 
-            string fileName = args[0];
+            string path = args[0];
+
+            if (Directory.Exists(path))
+            {
+                DirectoryProcess(path);
+                return;
+            }
+            else
+                ProfileCsvFile(path);
+            
+        }
+
+        static void DirectoryProcess(string pathName)
+        {
+            Directory.GetFiles(pathName, "*.csv")
+                .ToList()
+                .ForEach(file => ProfileCsvFile(file));
+        }
+
+        static void ProfileCsvFile(string fileName)
+        {
             string header = File.ReadLines(fileName).FirstOrDefault() ?? "";
             int numberOfLines = File.ReadLines(fileName).Count();
             int dataRowCount = Math.Max(numberOfLines - 1, 0);
@@ -25,6 +46,7 @@
             Console.WriteLine($"Header: {header}");
             Console.WriteLine($"Line count: {numberOfLines}");
             Console.WriteLine($"Data row count: {dataRowCount}");
+            Console.WriteLine();
         }
     }
 }
