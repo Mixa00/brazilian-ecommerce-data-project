1e73d020771d13ac62013b2542fc8fd8633b8b80
Mihajlo Stankovic <mihajlo.stankovic1212@gmail.com>
2026-07-10 09:52:34 +0200

    Add average item price insight

--- sql/14_business_insights.sql
+++ sql/14_business_insights.sql
@@ -126,3 +126,19 @@ ORDER BY
 	total_revenue DESC;
 GO
 
+-- 8. Which product categories have the highest average item price?
+SELECT TOP 10
+	p.product_category_name_english,
+	COUNT(*) AS order_item_count,
+	SUM(oi.price) AS total_revenue,
+	AVG(oi.price) AS average_item_price
+FROM
+	dw.fact_order_items AS oi
+	JOIN dw.dim_products AS p ON oi.product_key = p.product_key
+GROUP BY
+	p.product_category_name_english
+HAVING
+	COUNT(*) >= 100
+ORDER BY
+	average_item_price DESC;
+GO
